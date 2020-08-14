1a7b0b1de76c9e4a0ed9c2e28316cf112e89a870
ziyanwould <448736378@qq.com>
2020-08-14 17:36:13 +0800

    1.对象合并函数 自定义模板引擎解释函数

--- plug.js
+++ plug.js
@@ -1,53 +1,52 @@
-/*
-JavaScript中，万物皆对象，所有对象都是继承自原型。JS在创建对象（不论是普通对象还是函数对象）的时候，都有一个叫做__proto__的内置属性，用于指向创建它的函数对象的原型对象prototype。
-关于原型问题，感兴趣的同学可以看这篇：js原型链
-在上面的需求中，我们可以将plugin对象改为原型的方式，则需要将plugin写成一个构造方法，我们将插件名换为Calculate避免因为Plugin大写的时候与Window对象中的API冲突。
-*/ 
+/**
+ *  接下来，我们开始编写我们的交互插件。
+    我们假设组件的弹出层就是一个对象。则这个对象是包含了我们的交互、样式、结构及渲染的过程。于是我们定义了一个构造方法：
+    function MyDialog(){} // MyDialog就是我们的组件对象了
+    对象MyDialog就相当于一个绳子，我们只要往这个绳子上不断地挂上钩子就是一个组件了。于是我们的组件就可以表示为：
 
-//假设我们的插件是对初始化参数进行运算并只输出结果，我们可以稍微改一下：
-
-// plugin.js
-// plugin.js
-;(function(undefined) {
-    "use strict"
-    var _global;
-
-    function result(args,type){
-        var argsArr = Array.prototype.slice.call(args);
-        if(argsArr.length == 0) return 0;
-        switch(type) {
-            case 1: return argsArr.reduce(function(p,c){return p + c;});
-            case 2: return argsArr.reduce(function(p,c){return p - c;});
-            case 3: return argsArr.reduce(function(p,c){return p * c;});
-            case 4: return argsArr.reduce(function(p,c){return p / c;});
-            case 5: return argsArr.reduce(function(p,c){return p % c;});
-            default: return 0;
-        }
+    function MyDialog(){}
+    MyDialog.prototype = {
+        constructor: this,
+        _initial: function(){},
+        _parseTpl: function(){},
+        _parseToDom: function(){},
+        show: function(){},
+        hide: function(){},
+        css: function(){},
+        ...
     }
+    然后就可以将插件的功能都写上。不过中间的业务逻辑，需要自己去一步一步研究。无论如何写，我们最终要做到通过实例化一个MyDialog对象就可以使用我们的插件了。
+    在编写的过程中，我们得先做一些工具函数：
+ */
 
-    function Calculate(){}
-    Calculate.prototype.add = function(){console.log(result(arguments,1));return this;}
-    Calculate.prototype.sub = function(){console.log(result(arguments,2));return this;}
-    Calculate.prototype.mul = function(){console.log(result(arguments,3));return this;}
-    Calculate.prototype.div = function(){console.log(result(arguments,4));return this;}
-    Calculate.prototype.sur = function(){console.log(result(arguments,5));return this;}
+ //对象合并
+ function extend(o,n,override) {
+     for(var key in n){
+         if(n.hasOwnProperty(key) && (!o.hasOwnProperty(key) || override)){
+             o[key]=n[key];
+         }
+     }
+     return o;
+ }
 
-
-    // 最后将插件对象暴露给全局对象
-    _global = (function(){ return this || (0, eval)('this'); }());
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = Calculate;
-    } else if (typeof define === "function" && define.amd) {
-        define(function(){return Calculate;});
-    } else {
-        !('Calculate' in _global) && (_global.Calculate = Calculate);
+ //自定义模板引擎解释函数
+ function templateEngine(html,data){
+     var re = /<%([^%>]+)?%>/g,
+        reExp = /(^()?(if|for|else|switch|case|break|{|}))(.*)?/g,
+        code = 'var r=[];\n',
+        cursor = 0;
+    var match;
+    var add = function(line,js) {
+        js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+            return add;
     }
-}());
+    while(match = re.exec(html)){
+        add(html.slice(cursor,match.index))(match[1],true);
+        cursor = match.index + match[0].length;
+    }
+    add(html.substr(cursor,html.length - cursor));
+    code += 'return r.join("");';
+    return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
 
-var plugin = new Calculate();
-plugin
-    .add(2,1)
-    .sub(2,1)
-    .mul(2,1)
-    .div(2,1)
-    .sur(2,1);
+ }
